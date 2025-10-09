20d4d67ac520fea01f1b150b25dd004d562596a4
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 14:09:44 -0300

    fix: pass arguments to signal call

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -17,8 +17,9 @@ func _get_is_flying() -> bool
 
 
 func _on_attack_area_2d_body_entered(body: Node2D) -> void:
-	_get_animator().animation_finished.connect(_on_attack_animation_ended)
+	_get_animator().animation_finished.connect(_on_attack_animation_ended.bind(_get_animator()))
 	_get_animator().play(_get_attack_animation_name())
+	
 	#TODO: play attack sound effect
 
 
