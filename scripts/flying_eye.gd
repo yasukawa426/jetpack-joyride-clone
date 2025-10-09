945c552a45d57923499c578792c11799f6fd1527
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 12:57:46 -0300

    refact: change flying eye to inherit Monsters and implement abstract functions

--- scripts/flying_eye.gd
+++ scripts/flying_eye.gd
@@ -1,6 +1,13 @@
-extends Node2D
+extends Monster
 
-signal hit
+func _get_animator() -> AnimatedSprite2D:
+	return $AnimatedSprite2D
 
-func _on_body_entered(body: Node2D) -> void:
-	hit.emit()
+
+func _get_attack_animation_name() -> String:
+	const ATTACK_STRINGS: Array[String] = ["attack", "attack2"]
+	
+	return  ATTACK_STRINGS[randi() % ATTACK_STRINGS.size()]
+	
+func _get_idle_animation_name() -> String:
+	return "idle"
