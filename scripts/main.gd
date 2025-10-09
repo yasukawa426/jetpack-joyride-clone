4fe22dc09e43d3a8a09553256e6b236f41e3a352
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 16:36:26 -0300

    feat: implement difficutly increase based on score

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,8 +5,14 @@ const PLAYER_START_POSITION := Vector2i(133, 424)
 const START_RUNNING_SPEED := 7
 const MAX_RUNNING_SPEED := 25
 
-const OBSTACLE_SPAWN_X := 1300
+const OBSTACLE_SPAWN_X := 1600
 const OBSTACLE_TYPES: Array[String] = ["res://scenes/obstacles/flying_eye.tscn", "res://scenes/obstacles/goblin.tscn"]
+
+const START_TIMER_LENGTH := 3
+const MINIMUM_TIMER_LENGTH := 0.5
+
+const START_TIMER_THRESHOLD: int = 100
+const START_SPAWNER_THRESHOLD: int = 350
 
 const SAVE_PATH := "user://highscore.json"
 
@@ -25,17 +31,25 @@ var loaded := false
 ## Foreground and background current position
 var foreground_scroll = 0
 ## Ground scroll speed
-var running_speed: int = 7
+var running_speed: float = 7
 
 var score: float = 0
 var highscore := 0
 
+#Amount of score needed to 
+#decrease timer maximum time (increasing spawn rate)
+var next_timer_threshold: int
+#increase number of timer spawners
+var next_spawner_threshold: int
 
 #TODO: Change some variables to global
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_window().size
+	
+	next_spawner_threshold = START_SPAWNER_THRESHOLD
+	next_timer_threshold = START_TIMER_THRESHOLD
 	
 	_load()
 	
@@ -51,7 +65,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not running:
 		$Background._set_scrolling_speed(0)
-		$ObstacleTimer.stop()
+		
+		for timer in $Timers.get_children():
+			timer.stop()
 		
 		#for obstacle in $Obstacles.get_children():
 		#	obstacle.queue_free()
@@ -78,14 +94,24 @@ func _input(event: InputEvent) -> void:
 
 	if not running:
 		if event.is_action_released("jump"):
+			$AnimationPlayer.play("day_cycle")
 			$UI/StartLabel.hide()
 			_set_running(true)
+			_add_new_timer()
 		
 
 func _set_score(value: float) -> void:
 	score = value
 	$UI/ScoreLabel.set_score(int(value))
 	$UI/GameoverUI/OverScoreLabel.set_score(int(value))
+	
+	if score > next_timer_threshold:
+		next_timer_threshold += START_TIMER_THRESHOLD
+		_update_running_difficulty()
+	
+	if score > next_spawner_threshold:
+		next_spawner_threshold += START_SPAWNER_THRESHOLD
+		_update_spawner_difficulty()
 
 func _set_highscore(value: int) -> void:
 	if value > highscore:
@@ -99,7 +125,8 @@ func _set_running(value: bool) -> void:
 	$Player.running = value
 	
 	if running == true:
-		$ObstacleTimer.start()		
+		for timer in $Timers.get_children():
+			timer.start()		
 
 ## Reset to initial state of the game when first started
 func reset():
@@ -109,10 +136,17 @@ func reset():
 	
 	for obstacle in $Obstacles.get_children():
 			obstacle.queue_free()
+			
+	for timer in $Timers.get_children():
+		timer.queue_free()
 	
 	$Obstacles.scrolling = true
 	$UI/StartLabel.show()
 	$UI/GameoverUI.hide()
+	
+	next_timer_threshold = START_TIMER_THRESHOLD
+	next_spawner_threshold = START_SPAWNER_THRESHOLD
+
 	
 	_set_score(0)
 	_set_running(false)
@@ -194,3 +228,18 @@ func _load():
 	var data:Dictionary = json.data
 	_set_highscore(data.get("highscore", 0))
 	print("Loaded highscore: " + str(highscore))
+
+
+func _update_running_difficulty():
+	running_speed += 0.5
+
+func _update_spawner_difficulty():
+	_add_new_timer()	
+
+func _add_new_timer() -> void:
+	var timer := Timer.new()
+	$Timers.add_child(timer)
+	timer.wait_time = START_TIMER_LENGTH
+	timer.one_shot = false
+	timer.start()
+	timer.timeout.connect(_on_obstacle_timer_timeout)
