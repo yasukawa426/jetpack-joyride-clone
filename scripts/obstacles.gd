550df22882bf321db518b6e1d32ddf25d6641acc
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 13:45:32 -0300

    refact: add scrolling check to obstacle

--- scripts/obstacles.gd
+++ scripts/obstacles.gd
@@ -1,7 +1,12 @@
 extends Node
+var scrolling: bool = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if not scrolling:
+		return
+	
 	for obstacle in get_children():
 		obstacle.position.x -= $"..".running_speed * delta * 60
 		
