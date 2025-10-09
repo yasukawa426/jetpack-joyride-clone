26cceaeea28749d2b0c000508ea753fd3738881f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 14:38:50 -0300

    feat: create setter for ui score and highscore

---
+++ scripts/over_high_score_label.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func set_high_score(value: int):
+	self.text = "HighScore: " + str(value)
---
+++ scripts/over_score_label.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func set_score(value: int):
+	self.text = "Score: " + str(value)
