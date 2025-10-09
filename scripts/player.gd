87834c53f5b036f9c4d1f2cd74b6f7bb8f0ba6d8
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2025-10-09 13:47:31 -0300

    feat/refact: implement initial player death and refact process funcion

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,7 @@ var screen_size: Vector2i
 ## Wheter we are in a run
 var running := false
 ## Are we dead????
-var dead := false
+var _dead := false
 
 
 func _ready() -> void:
@@ -20,42 +20,66 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	# 
-	if not running and not dead:
-		$AnimatedSprite2D.play("idle")
-		return
-		
-	if is_on_floor():
-		$AnimatedSprite2D.speed_scale = 1
-		$AnimatedSprite2D.play("run")
-	
+	#Stuff here happens if the player is alive or not
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		
 		if velocity.y > FALL_MAX_VELOCITY:
 			velocity.y = FALL_MAX_VELOCITY
+
+	move_and_slide()
+	position.y = clampi(position.y, 0, screen_size.y)
+	
+	#stuff to do only if dead
+	if _dead:
+		#right now, nothing. so we are returning instead of having a nested if else
+		return
 		
+	#stuff to do only if alive
+	_handle_input(delta)
+		
+	if not running:
+		$AnimatedSprite2D.play("idle")
+		return
+		
+	if is_on_floor():
+		$AnimatedSprite2D.speed_scale = 1
+		$AnimatedSprite2D.play("run")
+	else:
 		if velocity.y < 0:
 			$AnimatedSprite2D.play("jump")
 		else:	
 			$AnimatedSprite2D.play("fall")
-		
 		# lets scale animation speed based on y velocity - we get the current percentage of the maximum value and then multipiply by maximum speed scale we want i.e 2
 		$AnimatedSprite2D.speed_scale = (abs(velocity.y) / abs(JUMP_MAX_VELOCITY)) * 2
+	 
 
+	
+	
+
+# Resets player status
+func reset():
+	running = false
+	_dead = false
+	
+func set_dead(value: bool):
+	if _dead == value:
+		#if not changing, does nothing. just in case we proc death multiple times in a row
+		return
+	
+	_dead = value
+	
+	if value == true:
+		#TODO: blood effect(?), rotate?
+		$AnimatedSprite2D.play("death")
+		pass
+
+
+func _handle_input(delta: float):
 	# Handle jump.
 	if Input.is_action_pressed("jump"):
 		velocity.y += JUMP_ACCELERATION * delta 
 		
 		if velocity.y < JUMP_MAX_VELOCITY:
 			velocity.y = JUMP_MAX_VELOCITY
-
-	move_and_slide()
-	position.y = clampi(position.y, 0, screen_size.y)
-	
-
-# Resets player status
-func reset():
-	running = false
-	dead = false
